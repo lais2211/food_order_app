a062fe70c6fcf019656a6c59a20ff0477bfcfd55
Lais_Sousa <lais.sousa1608@gmail.com>
2024-04-02 21:04:24 -0300

    feat: criacao de componentes

---
+++ lib/src/modules/view/components/login_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class LoginButton extends StatelessWidget {
+  final Function()? onTap;
+  final String text;
+  const LoginButton({super.key, this.onTap, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).colorScheme.inversePrimary,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
