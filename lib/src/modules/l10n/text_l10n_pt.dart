81c12d310485cc326f94f8049a3ffb3665d30237
Lais_Sousa <lais.sousa1608@gmail.com>
2024-04-02 21:04:06 -0300

    feat: criacao de l10n

---
+++ lib/src/modules/l10n/text_l10n_pt.dart
@@ -0,0 +1,12 @@
+class TextL10nPt{
+  String get loginTitle => 'Restaurante App';
+  String get email => 'Email';
+  String get password => 'Senha';
+  String get loginButton => 'Entrar';
+  String get notMember => 'Não é cadastrado?';
+  String get registerMember => 'Se registre';
+  String get createAccount => 'Vamos criar uma conta para você';
+  String get confirmPassword => 'Confirme sua senha';
+  String get haveAccount => 'Já tem conta?';
+  String get login => 'Faça login';
+}
