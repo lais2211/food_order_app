0ce66569c2b0dcec10750d6b334a1e8c9c1f9f90
Lais_Sousa <lais.sousa1608@gmail.com>
2024-04-03 17:20:12 -0300

    feat: criação de novos componentes

---
+++ lib/src/modules/view/components/home_drawer.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:restaurante_app/src/modules/view/components/home_drawer_tile.dart';
+import 'package:restaurante_app/src/modules/view/pages/home_page.dart';
+import 'package:restaurante_app/src/modules/view/pages/login_page.dart';
+
+import '../../l10n/text_l10n_pt.dart';
+import '../pages/settings_page.dart';
+
+class HomeDrawer extends StatelessWidget {
+  const HomeDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextL10nPt text = TextL10nPt();
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 100.0,
+            ),
+            child: Icon(
+              Icons.restaurant,
+              size: 80.0,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(
+              25.0,
+            ),
+            child: Divider(
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+          HomeDrawerTile(
+            text: text.home,
+            icon: Icons.home,
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomePage(),
+                ),
+              );
+            },
+          ),
+          HomeDrawerTile(
+            text: text.settings,
+            icon: Icons.settings,
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsPage(),
+                ),
+              );
+            },
+          ),
+          const Spacer(),
+          HomeDrawerTile(
+            text: text.logout,
+            icon: Icons.logout,
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const LoginPage(),
+                ),
+              );
+            },
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/modules/view/components/home_drawer_tile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomeDrawerTile extends StatelessWidget {
+  final String text;
+  final IconData? icon;
+  final void Function()? onTap;
+
+  const HomeDrawerTile({super.key, required this.text, this.icon, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 25.0,
+      ),
+      child: ListTile(
+        title: Text(
+          text,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.inversePrimary,
+          ),
+        ),
+        leading: Icon(
+          icon,
+          color: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        onTap: onTap,
+      ),
+    );
+  }
+}
