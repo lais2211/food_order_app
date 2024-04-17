d955fd1554884461df0f0e15747ac7a2d45ab1da
Lais_Sousa <lais.sousa1608@gmail.com>
2024-04-17 11:58:57 -0300

    chore: adição de dependencias

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurante_app/src/modules/data/models/restaurant_model.dart';
+import 'package:restaurante_app/src/modules/view/controllers/cart_controller.dart';
+import 'package:restaurante_app/src/modules/view/controllers/food_page_controller.dart';
 import 'package:restaurante_app/src/theme/theme_provider.dart';
 
 import 'my_app.dart';
@@ -18,6 +20,12 @@ void main() {
         ),
         ChangeNotifierProvider(
           create: (context) => Restaurant(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => CartController(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => FoodPageController(),
         ),
       ],
       child: const MyApp(),
--- lib/src/config/dependencies_config.dart
+++ lib/src/config/dependencies_config.dart
@@ -1,6 +1,8 @@
 import 'package:get_it/get_it.dart';
+import 'package:restaurante_app/src/modules/view/controllers/cart_controller.dart';
 import 'package:restaurante_app/src/modules/view/controllers/food_page_controller.dart';
 import 'package:restaurante_app/src/modules/view/controllers/login_page_controller.dart';
+import 'package:restaurante_app/src/modules/view/controllers/payment_page_controller.dart';
 import 'package:restaurante_app/src/modules/view/controllers/register_page_controller.dart';
 
 import '../modules/view/controllers/home_page_controller.dart';
@@ -13,4 +15,6 @@ void registerDependencies() {
       LoginOrRegisterController());
   GetIt.I.registerSingleton<HomePageController>(HomePageController());
   GetIt.I.registerSingleton<FoodPageController>(FoodPageController());
+  GetIt.I.registerSingleton<PaymentPageController>(PaymentPageController());
+  GetIt.I.registerSingleton<CartController>(CartController());
 }
