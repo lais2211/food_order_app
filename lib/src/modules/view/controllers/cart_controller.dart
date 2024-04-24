114c92e6807d4246577b9ce83b371cbc9a02b4b4
Lais_Sousa <lais.sousa1608@gmail.com>
2024-04-24 18:59:57 -0300

    feat: criacao de novos metodos

--- lib/src/modules/view/controllers/cart_controller.dart
+++ lib/src/modules/view/controllers/cart_controller.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/foundation.dart';
+import 'package:intl/intl.dart';
 import 'package:mobx/mobx.dart';
 import 'package:restaurante_app/src/modules/data/models/addon_model.dart';
 import 'package:restaurante_app/src/modules/data/models/cart_model.dart';
 import '../../data/models/food_model.dart';
-
 
 part 'cart_controller.g.dart';
 
 class CartController = _CartControllerBase with _$CartController;
 
 abstract class _CartControllerBase extends ChangeNotifier with Store {
-
-   @observable
-   ObservableList<CartModel> cartList = ObservableList<CartModel>();
+  @observable
+  ObservableList<CartModel> cartList = ObservableList<CartModel>();
 
   @observable
   late CartModel cart;
-  
+
   @computed
   double get totalPriceCart {
     double addonsPrice =
@@ -73,25 +72,25 @@ abstract class _CartControllerBase extends ChangeNotifier with Store {
     });
   }
 
-
   @action
   double getTotalPriceRestaurant() {
-  double total = 0.0;
+    double total = 0.0;
 
-  for (CartModel cartModel in cartList) {
-    double itemTotal = cartModel.food.price;
+    for (CartModel cartModel in cartList) {
+      double itemTotal = cartModel.food.price;
 
-    if (cartModel.selectedAddons != null) {
-      for (Addon addon in cartModel.selectedAddons!) {
-        itemTotal += addon.price;
+      if (cartModel.selectedAddons != null) {
+        for (Addon addon in cartModel.selectedAddons!) {
+          itemTotal += addon.price;
+        }
       }
+
+      total += itemTotal * cartModel.quantity;
     }
 
-    total += itemTotal + cartModel.quantity;
+    return total;
   }
 
-  return total;
-}
   @action
   int getTotalItemCount() {
     int totalItemCount = 0;
@@ -102,4 +101,55 @@ abstract class _CartControllerBase extends ChangeNotifier with Store {
     return totalItemCount;
   }
 
+  @action
+  String displayCartReceipt() {
+    final receipt = StringBuffer();
+
+    receipt.writeln('Aqui está o seu recibo.');
+    receipt.writeln();
+
+    String formattedDate = DateFormat('yyyy-MM-dd                 HH:mm:ss')
+        .format(DateTime.now());
+
+    receipt.writeln(formattedDate);
+    receipt.writeln();
+    receipt.writeln('----------------');
+
+    for (final cartItem in cartList) {
+      receipt.writeln(
+          "${cartItem.quantity} X ${cartItem.food.name} - ${formatPrice(cartItem.food.price)}");
+      if (cartItem.selectedAddons!.isNotEmpty) {
+        receipt.writeln('Adicionais: ${formatAddons(cartItem.selectedAddons)}');
+        receipt.writeln();
+      }
+      receipt.writeln('----------------');
+      receipt.writeln();
+      receipt.writeln('Total de items: ${getTotalItemCount()}');
+      receipt.writeln('Preço total: ${formatPrice(getTotalPriceRestaurant())}');
+    }
+    return receipt.toString();
+  }
+
+  @action
+  String formatPrice(double price) {
+    return 'R\$${price.toStringAsFixed(2)}';
+  }
+
+  @action
+  String formatAddons(List<Addon>? addons) {
+    return addons!
+        .map((addon) => '${addon.name} (${formatPrice(addon.price)})')
+        .join(',');
+  }
+
+  @action
+  String deliveyTime() {
+    DateTime now = DateTime.now();
+    DateTime dateTimePlus30Minutes = now.add(const Duration(minutes: 30));
+
+    String formattedDate =
+        DateFormat('yyyy-MM-dd HH:mm:ss').format(dateTimePlus30Minutes);
+
+    return 'Entrega estimada para $formattedDate';
+  }
 }
